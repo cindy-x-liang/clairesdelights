782322ea03a61f725998f0793714029e548cd43b
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-08-04 11:13:07 -0600

    updated email service

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,37 +1,41 @@
 // basically going to allow users to see order summary, select payment method
 
-import React, { useRef, useContext }  from 'react';
+import React, { useRef, useContext } from 'react';
 import emailjs from '@emailjs/browser';
-import {ShopContext} from "../../context/shop-context"
-import {CartItemCheckout} from '../cart/cart-item-checkout'
+import { ShopContext } from "../../context/shop-context"
+import { CartItemCheckout } from '../cart/cart-item-checkout'
 import { PRODUCTS } from "../../products";
 import './checkout.css'
-import {useNavigate} from 'react-router-dom'
-export const Checkout = () =>{
-    const {cartItems,getTotalCartAmount} = useContext(ShopContext);
+import { useNavigate } from 'react-router-dom'
+
+export const Checkout = () => {
+    const { cartItems, getTotalCartAmount } = useContext(ShopContext);
     const totalAmount = getTotalCartAmount()
     const navigate = useNavigate()
     const form = useRef();
+    //const history = useHistory();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    emailjs.sendForm('service_sx2299h', 'template_t0ds3yr', form.current, 'ST-zuq4fAr0ro3Znq')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
-    return <div className = "checkout">
-        <div className = "orderSum">
+        emailjs.sendForm('service_sx2299h', 'template_t0ds3yr', form.current, 'ST-zuq4fAr0ro3Znq')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log('here');
+                console.log(error.text);
+            });
+        navigate('/clairesdelights/orderplaced');
+    };
+    return <div className="checkout">
+        <div className="orderSum">
             Order Summary:
-            <div className = "cartItems">
-            {PRODUCTS.map((product)=>{
-                if (cartItems[product.id]!== 0){
-                    return <CartItemCheckout data={product}/>
-                }
-            })}
+            <div className="cartItems">
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItemCheckout data={product} />
+                    }
+                })}
             </div>
         </div>
 
@@ -39,38 +43,38 @@ export const Checkout = () =>{
             <div>
                 Order Information:
             </div>
-      <label>Name</label>
-      <br />
-      <input  type="text" name="to_name" />
-      <br />
-      <br />
-      <label>Email</label>
-      <br />
-      <input type="email" name="from_name" />
-      <br />
-      <br />
-      <label>Shipping Address</label>
-      <br />
-      <textarea name="message" />
-      <br />
-      <br />
-      <p>Choose payment type:</p>
-    
-        <input type="radio" value="Male" name="zelle" /> Zelle: 6507876333
-        <br />
-        <input type="radio" value="Female" name="zelle" /> Venmo: cindyliang_
-    
-        <br />
-       <br />
-      <label>
-        We charge a flat fee shipping of $6 for all packages unless you live in the DFW area.
-        <br />
-        <br />
-       <b>Order Total: ${totalAmount+6}</b> </label>
-        <br />
-        <br />
-        <div></div>
-      <input type="submit" value="Submit Order" onClick={() => navigate('/clairesdelights/orderplaced')}/>
-    </form>
+            <label>Name</label>
+            <br />
+            <input type="text" name="to_name" />
+            <br />
+            <br />
+            <label>Email</label>
+            <br />
+            <input type="email" name="from_name" />
+            <br />
+            <br />
+            <label>Shipping Address</label>
+            <br />
+            <textarea name="message" />
+            <br />
+            <br />
+            <p>Choose payment type:</p>
+
+            <input type="radio" value="Male" name="zelle" /> Zelle: 6507876333
+            <br />
+            <input type="radio" value="Female" name="zelle" /> Venmo: cindyliang_
+
+            <br />
+            <br />
+            <label>
+                We charge a flat fee shipping of $6 for all packages unless you live in the DFW area.
+                <br />
+                <br />
+                <b>Order Total: ${totalAmount + 6}</b> </label>
+            <br />
+            <br />
+            <div></div>
+            <input type="submit" value="Submit Order" />
+        </form>
     </div>
 };
